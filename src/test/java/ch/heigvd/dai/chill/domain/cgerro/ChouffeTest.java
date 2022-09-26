18c73db64964725530e3d67af454e941967a6d4b
Céline Roger <roger.celine93@gmail.com>
2022-09-26 15:41:59 +0200

    Add chouffe beer with test

---
+++ src/test/java/ch/heigvd/dai/chill/domain/cgerro/ChouffeTest.java
@@ -0,0 +1,32 @@
+package ch.heigvd.dai.chill.domain.cgerro;
+
+import ch.heigvd.dai.chill.domain.Bartender;
+import ch.heigvd.dai.chill.domain.cgerro.Chouffe;
+import ch.heigvd.dai.chill.protocol.OrderRequest;
+import ch.heigvd.dai.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ChouffeTest {
+
+  @Test
+  void thePriceAndNameForChouffeShouldBeCorrect() {
+    Chouffe beer = new Chouffe();
+    assertEquals(beer.getName(), Chouffe.NAME);
+    assertEquals(beer.getPrice(), Chouffe.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForChouffe() {
+    Bartender roman = new Bartender();
+    String productName = "ch.heigvd.dai.chill.domain.cgerro.Chouffe";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = roman.order(request);
+    BigDecimal expectedTotalPrice = Chouffe.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
